37dc1473d12635c7f378bc5cd92296c855411dac
herreang000 <herreang000@warren.k12.in.us>
2024-09-25 11:32:23 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,23 @@
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-
 public class Main {
+
     public static void main(String[] args) {
-        BigInteger number = new BigInteger("93983983983938983839839439789785787489783479479024304093049048945945945089595894805849850485930948958495803");
-        BigInteger number2 = new BigInteger("4095098940985095095857589479578597560195753450156407515604315485601510454675648755680753018568057578650475604");
+
+
+        BigInteger number = new BigInteger("36554651465432168765196874654168765132165765321654654654321324");
+        BigInteger number2 = new BigInteger("654635419668765496876543541657654765416876476354168765465796546546876546576547654657657432135468798765465");
         BigInteger number3 = number.add(number2);
         BigInteger number4 = number.subtract(number2);
-        System.out.println(number4);
-        System.out.println(number3);
         BigInteger gcd = number3.gcd(number4);
         System.out.println(gcd);
 
 
 
-        BigDecimal i = new BigDecimal("1123412341.123412341433456234523645345256");
-        BigDecimal i2 = new BigDecimal("14793738947895734923.4309483049830940394034930495256");
-        BigDecimal square = Math.pow(i, 1123412341.123412341433456234523645345256);
-        BigDecimal square2 = Math.pow(i2, 14793738947895734923.4309483049830940394034930495256);
-        System.out.println(square + square2);
+        BigDecimal decimal = new BigDecimal("1123412341.123412341433456234523645345256");
+        BigDecimal decimal2 = new BigDecimal("112334314343254354325412341.123412341433456234523645345256");
+        System.out.println(decimal.pow(2));
     }
+
 }
